21aab0a2d750ed2746376ce3fa3b6f52ee09be5c
Henrique Tavares <ihenrits@gmail.com>
2019-08-18 23:11:48 -0300

    Estilizaçao da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-lona-sapatofran-polo-masculino/03/HAP-0176-003/HAP-0176-003_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-lona-sapatofran-polo-masculino/03/HAP-0176-003/HAP-0176-003_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-lona-sapatofran-polo-masculino/03/HAP-0176-003/HAP-0176-003_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-lona-sapatofran-polo-masculino/03/HAP-0176-003/HAP-0176-003_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-lona-sapatofran-polo-masculino/03/HAP-0176-003/HAP-0176-003_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-lona-sapatofran-polo-masculino/03/HAP-0176-003/HAP-0176-003_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
