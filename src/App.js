d5a1509174633627475fb1966dcaa0804997ed5f
Henrique Tavares <ihenrits@gmail.com>
2019-08-19 22:53:26 -0300

    Configurando o Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
