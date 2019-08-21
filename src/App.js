9c922628bea68f737d7bca47c3310f64741bb5c6
Henrique Tavares <ihenrits@gmail.com>
2019-08-21 13:16:43 -0300

    Navegando no Saga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -10,17 +10,18 @@ import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
+import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <GlobalStyle />
         <Header />
         <Routes />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,6 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../util/format';
 
 import { addToCartSuccess, updateAmountSuccess } from './actions';
@@ -33,6 +34,7 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
