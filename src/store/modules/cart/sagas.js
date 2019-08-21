921183bd9e813427928c1b35df514a46a23a9efc
Henrique Tavares <ihenrits@gmail.com>
2019-08-21 12:53:16 -0300

    Estoque na adiçao

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,13 +1,28 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExits = yield select(state => state.cart.find(p => p.id === id));
 
-  yield put(addToCartSuccess(response.data));
+  if (productExits) {
+    const amount = productExits.amount + 1;
+
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
