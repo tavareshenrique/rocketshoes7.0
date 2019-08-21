2571df9d92dd5ad1c4a86f68b4508b3cbfc53158
Henrique Tavares <ihenrits@gmail.com>
2019-08-21 12:56:51 -0300

    Agora sim Estoque na adiçao

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -8,9 +8,19 @@ import { addToCartSuccess, updateAmount } from './actions';
 function* addToCart({ id }) {
   const productExits = yield select(state => state.cart.find(p => p.id === id));
 
-  if (productExits) {
-    const amount = productExits.amount + 1;
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExits ? productExits.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.log('ERRO');
+    return;
+  }
+
+  if (productExits) {
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
